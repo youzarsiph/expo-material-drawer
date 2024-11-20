f0ba2e7a74fa10889b4756e192af77f057c7b5a8
Yousef Abu Shanab <93343012+youzarsiph@users.noreply.github.com>
2024-11-20 00:51:19 -0800

    Initial commit

---
+++ ui/components/index.ts
@@ -0,0 +1,10 @@
+/**
+ * Components
+ */
+
+import HelloWave from '@/ui/components/HelloWave'
+import StackHeader from '@/ui/components/StackHeader'
+import TabBar from '@/ui/components/TabBar'
+import TabsHeader from '@/ui/components/TabsHeader'
+
+export { HelloWave, StackHeader, TabBar, TabsHeader }
